a99ec7b39a91b6a75b9f0d75d5aad0df3089ce4f
DavidTrabuco <davidtrabuco88@gmail.com>
2025-05-26 22:36:45 -0300

    Atualização Roteiro Faculdade

--- projetoEstudoOO/src/roteiro9/parte1/Quadrado.java
+++ projetoEstudoOO/src/roteiro9/parte1/Quadrado.java
@@ -1,6 +1,6 @@
-package roteiro9;
+package roteiro9.parte1;
 
-public class Quadrado {
+public class Quadrado implements FiguraGeometrica {
     private double lado;
 
     public Quadrado(double lado) {
@@ -11,6 +11,19 @@ public class Quadrado {
     }
     public void setlado(double lado) {
         this.lado = lado;
+    }
+
+    @Override
+    public String getNome() {
+        return "Quadrado";
+    }
+    @Override
+    public double getArea() {
+        return lado * lado;
+    }
+    @Override
+    public double getPerimetro() {
+        return 4 * lado;
     }
     
     
